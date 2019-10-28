a8ccba32a12250859d6c49fcf05e25581361df32
joedy23 <joedy23@aliyun.com>
2019-10-28 16:03:36 +0800

    谷粒商城创建ms系统

---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/GmallPmsApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallPmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallPmsApplication.class, args);
+    }
+
+}
